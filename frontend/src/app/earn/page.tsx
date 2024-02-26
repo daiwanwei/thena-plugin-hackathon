d35b6fc5edd5d589d18911970596af7affecc735
Wade <wade@teahouse.finance>
2024-02-26 12:53:42 +0800

    update ui

--- frontend/src/app/earn/page.tsx
+++ frontend/src/app/earn/page.tsx
@@ -2,10 +2,9 @@
 
 import React from "react";
 import {VaultActionCard} from "@/components/VaultActionCard";
-
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24 gap-2">
         <VaultActionCard/>
     </main>
   );
