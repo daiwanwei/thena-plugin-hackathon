a8280fda58e38184aed2b0aa5a2b5443134d2d73
Wade <wade@teahouse.finance>
2024-02-26 12:52:30 +0800

    add price library

--- frontend/src/utils/common.ts
+++ frontend/src/utils/common.ts
@@ -1,14 +1,24 @@
 import BigNumber from "bignumber.js";
 import { Decimal } from 'decimal.js';
+import {TickMath} from "@uniswap/v3-sdk";
 export function formatBalances(balances: string, decimals: number, fixed=2): string {
   return BigNumber(balances)
       .dividedBy(BigNumber(10 ** decimals))
       .toFixed(fixed);
 }
 
-export function formatPrice(priceSqrt: bigint,isToken0Price = true): string {
-   const price0=new Decimal(priceSqrt.toString()).dividedBy(new Decimal(2).pow(96)).toPrecision(3);
-   const price1=(new Decimal(1).dividedBy(price0)).toPrecision(3);
+export function formatAmount(amount: string, decimals: number): string {
+    return BigNumber(amount)
+        .multipliedBy(BigNumber(10 ** decimals))
+        .toFixed(0);
+}
+
+export function formatPrice(priceSqrt: bigint,isToken0Price = true,fixed=2): string {
+    if (priceSqrt === BigInt(0)) {
+        return "0";
+    }
+   const price0=new Decimal(priceSqrt.toString(10)).pow(2).dividedBy(new Decimal(2).pow(192)).toPrecision(fixed);
+   const price1=(new Decimal(1).dividedBy(price0)).toPrecision(fixed);
    return isToken0Price?price0:price1;
 }
 
@@ -16,7 +26,24 @@ export function encodePriceSqrt(price:BigNumber): bigint {
     return BigInt(
         price.sqrt()
             .multipliedBy(new BigNumber(2).pow(96))
-            .integerValue(3)
-            .toString()
+            .integerValue()
+            .toString(10)
     );
+}
+
+export function proximalPriceSqrt(targetSqrt: bigint, tick0:number=-887272,tick1:number=887272,tickSpacing:number=60): bigint {
+    if (tick0 > tick1) {
+        throw new Error("tick0 must be less than or equal to tick1");
+    }
+    if (tick1-tick0<=tickSpacing) {
+        return BigInt(TickMath.getSqrtRatioAtTick(tick0).toString(10))
+    }
+    const tick = Math.floor((tick0 + tick1) / 2/tickSpacing)*tickSpacing;
+    const priceSqrt = BigInt(TickMath.getSqrtRatioAtTick(tick).toString(10));
+    if (tick === tick1 || tick === tick0) {
+        return priceSqrt
+    }
+    return targetSqrt < priceSqrt?
+        proximalPriceSqrt(targetSqrt, tick0, tick, tickSpacing):
+        proximalPriceSqrt(targetSqrt, tick, tick1, tickSpacing);
 }
